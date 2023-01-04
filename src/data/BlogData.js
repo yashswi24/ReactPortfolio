13c067ac5d1b1b9fc54ce8d34af5a664c7be74e5
yashswi24 <yashswi2412@gmail.com>
2023-01-04 11:41:45 +0530

    Initial commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,30 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"What are the 14 creative business use cases that leverage the combination of business intelligence and machine learning.",
+        tags:["ML","Business","AI"],
+        date:"14 October, 2022",
+        imgSrc:"https://imgs.search.brave.com/BVY7yhs_IM_oZYmBhDvcMedgPekOu4aISf4f3G7bDEU/rs:fit:670:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC41/amV5UzRfdXJtMXNF/TVBEdHpiUTVRSGFG/UCZwaWQ9QXBp",
+        link:"https://paidforarticles.com/what-are-the-14-creative-business-use-cases-that-leverage-the-combination-of-business-intelligence-and-machine-learning-681671"
+    },
+    {
+        id:2,
+        name:"What Is Machine Learning?",
+        tags:["Machine Learning"],
+        date:"5 July, 2021",
+        imgSrc:"https://imgs.search.brave.com/F651wzhXjukYq4m9FBbIDTxc2gNe7xtgwdYNXxdH8Mg/rs:fit:860:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5M/OFFSbHNXMm8zSDcy/LVNRblZhMXlRSGFF/RiZwaWQ9QXBp",
+        link:"https://paidforarticles.com/what-is-machine-learning-3-681673"
+    },
+    {
+        id:3,
+        name:"How was my Internship Experience at OASIS INFOBYTE",
+        tags:["Oasis Infobyte","Web-Development","Intenship"],
+        date:"6 July, 2021",
+        imgSrc:"https://imgs.search.brave.com/l5KKwnRGrN7-hWZT6OptUjniE5n9Q6GKsDcxMCox2Hs/rs:fit:948:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4z/X2x5LVBoN0JRaElO/QXZqVko5V0VBSGFE/dCZwaWQ9QXBp",
+        link:"https://paidforarticles.com/what-was-my-internship-experience-at-oasis-infobyte-685355"
+    },
+    
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,43 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Data Analysis WebApp",
+        description:"It is build on top of the streamlit, with styledComponents.",
+        tags:["streamlit","Python","styledComponents"],
+        
+        demo:"https://github.com/yashswi24/Data-Analysis-Web-App",
+        github:"https://github.com/yashswi24/Data-Analysis-Web-App"
+    },
+    {
+        id:2,
+        name:"Image Denoising Application",
+        description:"It is an application which uses Autoencoders to remove noise from image.",
+        tags:["python","autoencoders"],
+        
+        demo:"https://github.com/yashswi24/Image-Denoising-Using-AutoEncoders",
+        github:"https://github.com/yashswi24/Image-Denoising-Using-AutoEncoders"
+        
+    },
+    {
+        id:3,
+        name:"Reactive Forms",
+        description:"It is build on top of the AngularJS, with styledComponents.",
+        tags:["angular","api","sass","Json-Server"],
+        
+        demo:"https://github.com/yashswi24/Reactive_Froms_Angular",
+        github:"https://github.com/yashswi24/Reactive_Froms_Angular"
+    },
+    {
+        id:4,
+        name:"React Portfolio",
+        description:"A cool looking Portfolio website build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://github.com/yashswi24",
+        github:"https://github.com/yashswi24"
+       
+    },
+  
+] 
